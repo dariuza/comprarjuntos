364b8d3c3b9a9fdb36c3dcb8c29503a5f890c76a
daruiza <daruiza@gmail.com>
2017-02-04 04:31:46 -0500

    Mejora codigo tienda

--- resources/views/comprarjuntos/tienda.blade.php
+++ resources/views/comprarjuntos/tienda.blade.php
@@ -657,16 +657,7 @@
 			        "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
 			    },
 			});
-		});
-
-		$( "#movil" ).keypress(function(evt) {
-			 evt = (evt) ? evt : window.event;
-		    var charCode = (evt.which) ? evt.which : evt.keyCode;
-		    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-		        return false;
-		    }
-		    return true;
-		});
+		});		
 
 		$( ".solo_numeros" ).keypress(function(evt) {
 			 evt = (evt) ? evt : window.event;
